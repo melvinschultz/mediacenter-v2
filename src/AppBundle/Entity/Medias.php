715d249785048d0374bb6230cebedb32cb95f198
melvinschultz <melvinschultzdev@gmail.com>
2016-08-15 19:52:06 +0200

    generate entity Medias and form MediasType

---
+++ src/AppBundle/Entity/Medias.php
@@ -0,0 +1,304 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Medias
+ *
+ * @ORM\Table(name="medias")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MediasRepository")
+ */
+class Medias
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="realisateurs", type="string", length=255)
+     */
+    private $realisateurs;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="acteurs", type="string", length=255)
+     */
+    private $acteurs;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="trailer", type="string", length=255)
+     */
+    private $trailer;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="annee", type="date")
+     */
+    private $annee;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="type", type="boolean")
+     */
+    private $type;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="status", type="boolean")
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Medias
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     * @return Medias
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string 
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set realisateurs
+     *
+     * @param string $realisateurs
+     * @return Medias
+     */
+    public function setRealisateurs($realisateurs)
+    {
+        $this->realisateurs = $realisateurs;
+
+        return $this;
+    }
+
+    /**
+     * Get realisateurs
+     *
+     * @return string 
+     */
+    public function getRealisateurs()
+    {
+        return $this->realisateurs;
+    }
+
+    /**
+     * Set acteurs
+     *
+     * @param string $acteurs
+     * @return Medias
+     */
+    public function setActeurs($acteurs)
+    {
+        $this->acteurs = $acteurs;
+
+        return $this;
+    }
+
+    /**
+     * Get acteurs
+     *
+     * @return string 
+     */
+    public function getActeurs()
+    {
+        return $this->acteurs;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Medias
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set trailer
+     *
+     * @param string $trailer
+     * @return Medias
+     */
+    public function setTrailer($trailer)
+    {
+        $this->trailer = $trailer;
+
+        return $this;
+    }
+
+    /**
+     * Get trailer
+     *
+     * @return string 
+     */
+    public function getTrailer()
+    {
+        return $this->trailer;
+    }
+
+    /**
+     * Set annee
+     *
+     * @param \DateTime $annee
+     * @return Medias
+     */
+    public function setAnnee($annee)
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    /**
+     * Get annee
+     *
+     * @return \DateTime 
+     */
+    public function getAnnee()
+    {
+        return $this->annee;
+    }
+
+    /**
+     * Set type
+     *
+     * @param boolean $type
+     * @return Medias
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return boolean 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     * @return Medias
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return boolean 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
---
+++ src/AppBundle/Form/MediasType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MediasType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('image')
+            ->add('realisateurs')
+            ->add('acteurs')
+            ->add('description')
+            ->add('trailer')
+            ->add('annee', 'date')
+            ->add('type')
+            ->add('status')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Medias'
+        ));
+    }
+}
